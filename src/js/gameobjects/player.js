021d9cb6d30af543c5beefac06f21a8ed2264796
Schansky, Christoph <christoph.schansky@isotravel.com>
2026-04-17 16:03:03 +0200

    turning improved, skidmarks

--- src/js/gameobjects/player.js
+++ src/js/gameobjects/player.js
@@ -1,31 +1,78 @@
 import {Car} from "./car.js";
+import stackDefCarMini from "../spritestacks/car_mini.js";
 import stackDefCarCoupe from "../spritestacks/car_coupe.js";
+import stackDefCarCabrio from "../spritestacks/car_cabrio.js";
+
+export const MINI = 0;
+export const COUPE = 1;
+export const CABRIO = 2;
+
+const stackdefs = [
+    stackDefCarMini,
+    stackDefCarCoupe,
+    stackDefCarCabrio
+];
 
 export class Player extends Car {
-    constructor({x,y,rot=0}) {
-        super({x,y,rot, stackdef: stackDefCarCoupe, maxspeed:600});
+    constructor({x,y,rot=0, cartype=MINI}) {
+        super({x,y,rot, stackdef: stackdefs[cartype], maxspeed:600});
         this.type = 'Player';
 
         this.vx = 0;
         this.vy = 0;
+        this.vr = 0;
+        this.vrMax = 2;
+        this.vrRate = 10;
+
+        this.skidmarks = 0;
     }
 
     update(deltaTime) {
         super.update(deltaTime);
+        this.skidmarks = 0;
         // handle input for rotation and acceleration
-        if(this.game.inputActions.turnLeft) {
-            this.rot -= Math.PI * deltaTime; // rotate left
-        }
-        if(this.game.inputActions.turnRight) {
-            this.rot += Math.PI * deltaTime; // rotate right
+        let turningPressed = false;
+        if(this.speed > 20) {
+            if(this.game.inputActions.turnLeft) {
+                this.vr -= this.vrRate * deltaTime;
+                turningPressed = true;
+                //this.rot -= Math.PI * deltaTime; // rotate left
+            } 
+            if(this.game.inputActions.turnRight) {
+                this.vr += this.vrRate * deltaTime;
+                turningPressed = true;
+                //this.rot += Math.PI * deltaTime; // rotate right
+            }
+            if(!turningPressed) {
+                this.vr *= 0.9;
+            } else {
+                this.skidmarks = 20;
+            }
+
+            if(this.vr > this.vrMax) {
+                this.vr = this.vrMax;
+            }
+            if(this.vr < -this.vrMax) {
+                this.vr = -this.vrMax;
+            }
+            if(this.vr < 0.01 && this.vr > -0.01) {
+                this.vr = 0;
+            } else {
+                this.rot += this.vr * deltaTime;
+            }
         }
         if(this.game.inputActions.accelerate) {
             this.speed += 200 * deltaTime; // accelerate
-            if(this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+            if(this.speed > this.maxSpeed) {
+                this.speed = this.maxSpeed;
+            } else {
+                this.skidmarks = 20;
+            }
         }
         if(this.game.inputActions.brake) {
             this.speed -= 300 * deltaTime; // brake
             if(this.speed < 0) this.speed = 0;
+            this.skidmarks = 20;
         }
         this.vx = Math.cos(this.rot) * this.speed;
         this.vy = Math.sin(this.rot) * this.speed;
@@ -42,7 +89,8 @@ export class Player extends Car {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.rot);
         ctx.beginPath();
-        ctx.fillStyle = '#00000009';
+        let alpha = this.skidmarks > 0 ? this.skidmarks.toString(16) : '03';
+        ctx.fillStyle = '#000000' + alpha;
         let x1 = (Math.random() * 4 - 2) - len/2;
         let y1 = 15 + (Math.random() * 4 - 2);
         let size1 = Math.random() * 2 + 1;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { Game } from "./js/game.js";
 import {Player} from "./js/gameobjects/player.js";
 import {desktrack} from "./js/tracks/desktrack.js";
-import {CABRIO, MINI, Npc} from "./js/gameobjects/npc.js";
+import {CABRIO, COUPE, MINI, Npc} from "./js/gameobjects/npc.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const game = new Game();
     game.generateBackground(desktrack);
     let x = desktrack.points[0][0];
     let y = desktrack.points[0][1];
-    let player = game.addGameObject(new Player({x, y}));
-    let cabrio = game.addGameObject(new Npc({x:x-100, y:y+20, cartype:CABRIO}));
+    let player = game.addGameObject(new Player({x, y, cartype:CABRIO}));
+    let cabrio = game.addGameObject(new Npc({x:x-100, y:y+20, cartype:COUPE}));
     let mini = game.addGameObject(new Npc({x:x+100, y:y-30, cartype:MINI}));
     game.setCameraTarget(player);
 });
